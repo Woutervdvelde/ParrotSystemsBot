74801da10b82747e8374882ceb849ea08959224b
Wouter van der Velde <woutervandervelde2000@hotmail.com>
2023-01-21 22:56:13 +0100

    calling correct methods, secured filenaming

--- tts.py
+++ tts.py
@@ -1,4 +1,5 @@
 import os
+import re
 from CoquiTTS.api import TTS as CoquiTTS
 from config import DEFAULT_TEXT_MODEL, DEFAULT_AUDIO_FOLDER
 from functools import cache
@@ -13,9 +14,9 @@ class TTS:
     def get_tts_model(model_name):
         return CoquiTTS(model_name)
 
-    # Returns a list of audio bytes
     def synthesize(self, text):
-        filepath = f'{DEFAULT_AUDIO_FOLDER}/{text}.wav'
+        filename = re.sub(r'\W+', '', text)
+        filepath = f'{DEFAULT_AUDIO_FOLDER}/{filename}.wav'
         self.model.tts_to_file(text=text, file_path=filepath)
         return filepath
 
